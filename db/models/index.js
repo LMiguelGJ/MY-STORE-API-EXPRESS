7a5ea9705058d4e5828429c5336e31b93494bf82
Luis Miguel <l.miguel.gonzalez.j@gmail.com>
2023-11-19 17:13:19 -0400

    Relaciones uno a uno

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,9 @@
 const {User, UserSchemas } = require('./user.model')
+const {Customer, CustomerSchema } = require('./customer.model')
 
 function setupModels(sequelize) {
     User.init(UserSchemas, User.config(sequelize))
+    Customer.init(CustomerSchema, Customer.config(sequelize))
 }
 
 module.exports = setupModels;
