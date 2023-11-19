761251c4867a1e452e8211d9ca894446d21d2b8e
Luis Miguel <l.miguel.gonzalez.j@gmail.com>
2023-11-18 22:21:24 -0400

    Configurando y corriendo migraciones con npm scripts

--- db/config.js
+++ db/config.js
@@ -2,15 +2,15 @@ const { config } = require('./../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
-    develomet: {
-        url: URI,
-        dialect: 'postgres',
-    },
-    production: {
-        url: URI,
-        dialect: 'postgres',
-    }
-};
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  }
+}
